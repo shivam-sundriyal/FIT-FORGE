c3d46caf7c4449feb2cc04a925727ec8217d864c
Shivam Sundriyal <shivamsundriyal22@gmail.com>
2024-02-01 21:25:54 +0530

    Added Pricing and BMI Calculating Sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,3 +36,63 @@ const scrollHeader = () => {
 }
 
 window.addEventListener('scroll', scrollHeader)
+
+/*=============== CALCULATE JS ===============*/
+const calculateForm = document.getElementById('calculate-form'),
+      calculateCm = document.getElementById('calculate-cm'),
+      calculateKg = document.getElementById('calculate-kg'),
+      calculateMessage = document.getElementById('calculate-message');
+
+const calculateBmi = (e) => {
+    e.preventDefault();
+
+    // Check if the fields have a value
+    if(calculateCm.value === '' || calculateKg.value == '') {
+        // Add and remove color
+        calculateMessage.classList.remove('color-green');
+        calculateMessage.classList.add('color-red');
+
+        // Show Message
+        calculateMessage.textContent = 'Fill in the Height and Weight 🧑‍💻';
+
+        // Remove Message after 3 seconds
+        setTimeout(() => {
+            calculateMessage.textContent = ''
+        }, 3000);
+    } else {
+        // BMI Formula
+        const cm = calculateCm.value / 100,
+              kg = calculateKg.value,
+              bmi = Math.round(kg / (cm * cm));
+
+        // Show your health status
+        if(bmi < 18.5) {
+            // Add color and display message
+            calculateMessage.classList.add('color-red');
+            calculateMessage.textContent = `Your BMI is ${bmi} and you are UNDERWEIGHT 😥`
+        } else if (bmi >= 18.5 && bmi <= 24.9) {
+          calculateMessage.classList.add("color-green");
+          calculateMessage.textContent = `Your BMI is ${bmi} and you are HEALTHY 🥳`;
+        } else if (bmi >= 25 && bmi <= 29.9) {
+          calculateMessage.classList.add("color-blue");
+          calculateMessage.textContent = `Your BMI is ${bmi} and you are OVERWEIGHT 😟`;
+        } else if (bmi >= 30 && bmi < 39.9) {
+          calculateMessage.classList.add("color-orange");
+          calculateMessage.textContent = `Your BMI is ${bmi} and you are OBESE 😰`;
+        } else {
+          calculateMessage.classList.add("color-red");
+          calculateMessage.textContent = `Your BMI is ${bmi} and you EXTREMELY OBESE  😱`;
+        }
+
+        // To clear the input field
+        calculateCm.value = '';
+        calculateKg.value = '';
+
+        // Remove Message after 4 seconds
+        setTimeout(() => {
+            calculateMessage.textContent = '';
+        }, 4000);
+    }
+}
+
+calculateForm.addEventListener('submit', calculateBmi)
