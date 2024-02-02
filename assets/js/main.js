7d1789f2e577de596bf9d0732a30f178fe39e189
Shivam Sundriyal <shivamsundriyal22@gmail.com>
2024-02-02 23:54:30 +0530

    Added EmailJS and Scroll Up

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,70 +29,159 @@ navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 const scrollHeader = () => {
-    const header = document.getElementById('header');
+  const header = document.getElementById("header");
 
-    this.scrollY >= 50 ? header.classList.add('bg-header')
-                       : header.classList.remove('bg-header');
-}
+  this.scrollY >= 50
+    ? header.classList.add("bg-header")
+    : header.classList.remove("bg-header");
+};
 
-window.addEventListener('scroll', scrollHeader)
+window.addEventListener("scroll", scrollHeader);
 
 /*=============== CALCULATE JS ===============*/
-const calculateForm = document.getElementById('calculate-form'),
-      calculateCm = document.getElementById('calculate-cm'),
-      calculateKg = document.getElementById('calculate-kg'),
-      calculateMessage = document.getElementById('calculate-message');
+const calculateForm = document.getElementById("calculate-form"),
+  calculateCm = document.getElementById("calculate-cm"),
+  calculateKg = document.getElementById("calculate-kg"),
+  calculateMessage = document.getElementById("calculate-message");
 
 const calculateBmi = (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    // Check if the fields have a value
-    if(calculateCm.value === '' || calculateKg.value == '') {
-        // Add and remove color
-        calculateMessage.classList.remove('color-green');
-        calculateMessage.classList.add('color-red');
+  // Check if the fields have a value
+  if (calculateCm.value === "" || calculateKg.value == "") {
+    // Add and remove color
+    calculateMessage.classList.remove("color-green");
+    calculateMessage.classList.add("color-red");
 
-        // Show Message
-        calculateMessage.textContent = 'Fill in the Height and Weight 🧑‍💻';
+    // Show Message
+    calculateMessage.textContent = "Fill in the Height and Weight 🧑‍💻";
 
-        // Remove Message after 3 seconds
-        setTimeout(() => {
-            calculateMessage.textContent = ''
-        }, 3000);
+    // Remove Message after 3 seconds
+    setTimeout(() => {
+      calculateMessage.textContent = "";
+    }, 3000);
+  } else {
+    // BMI Formula
+    const cm = calculateCm.value / 100,
+      kg = calculateKg.value,
+      bmi = Math.round(kg / (cm * cm));
+
+    // Show your health status
+    if (bmi < 18.5) {
+      // Add color and display message
+      calculateMessage.classList.add("color-red");
+      calculateMessage.textContent = `Your BMI is ${bmi} and you are UNDERWEIGHT 😥`;
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+      calculateMessage.classList.add("color-green");
+      calculateMessage.textContent = `Your BMI is ${bmi} and you are HEALTHY 🥳`;
+    } else if (bmi >= 25 && bmi <= 29.9) {
+      calculateMessage.classList.add("color-blue");
+      calculateMessage.textContent = `Your BMI is ${bmi} and you are OVERWEIGHT 😟`;
+    } else if (bmi >= 30 && bmi < 39.9) {
+      calculateMessage.classList.add("color-orange");
+      calculateMessage.textContent = `Your BMI is ${bmi} and you are OBESE 😰`;
     } else {
-        // BMI Formula
-        const cm = calculateCm.value / 100,
-              kg = calculateKg.value,
-              bmi = Math.round(kg / (cm * cm));
-
-        // Show your health status
-        if(bmi < 18.5) {
-            // Add color and display message
-            calculateMessage.classList.add('color-red');
-            calculateMessage.textContent = `Your BMI is ${bmi} and you are UNDERWEIGHT 😥`
-        } else if (bmi >= 18.5 && bmi <= 24.9) {
-          calculateMessage.classList.add("color-green");
-          calculateMessage.textContent = `Your BMI is ${bmi} and you are HEALTHY 🥳`;
-        } else if (bmi >= 25 && bmi <= 29.9) {
-          calculateMessage.classList.add("color-blue");
-          calculateMessage.textContent = `Your BMI is ${bmi} and you are OVERWEIGHT 😟`;
-        } else if (bmi >= 30 && bmi < 39.9) {
-          calculateMessage.classList.add("color-orange");
-          calculateMessage.textContent = `Your BMI is ${bmi} and you are OBESE 😰`;
-        } else {
-          calculateMessage.classList.add("color-red");
-          calculateMessage.textContent = `Your BMI is ${bmi} and you EXTREMELY OBESE  😱`;
-        }
-
-        // To clear the input field
-        calculateCm.value = '';
-        calculateKg.value = '';
-
-        // Remove Message after 4 seconds
-        setTimeout(() => {
-            calculateMessage.textContent = '';
-        }, 4000);
+      calculateMessage.classList.add("color-red");
+      calculateMessage.textContent = `Your BMI is ${bmi} and you EXTREMELY OBESE  😱`;
     }
-}
 
-calculateForm.addEventListener('submit', calculateBmi)
+    // To clear the input field
+    calculateCm.value = "";
+    calculateKg.value = "";
+
+    // Remove Message after 4 seconds
+    setTimeout(() => {
+      calculateMessage.textContent = "";
+    }, 4000);
+  }
+};
+
+calculateForm.addEventListener("submit", calculateBmi);
+
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById("contact-form"),
+  contactMessage = document.getElementById("contact-message"),
+  contactUser = document.getElementById("contact-user");
+
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  // Check if field has value
+  if (contactUser.value === "") {
+    // Add and Remove color
+    contactMessage.classList.remove("color-green");
+    contactMessage.classList.add("color-red");
+
+    // Show message
+    contactMessage.textContent = "You must enter your email";
+
+    // Remove message after 3 seconds
+    setTimeout(() => {
+      contactMessage.textContent = "";
+    }, 3000);
+  } else {
+    // serviceID, templateID, #form, publicKey
+    emailjs
+      .sendForm(
+        "service_g23kfyh",
+        "template_50szop5",
+        "#contact-form",
+        "SUE3iGSvmIUFkFZGM"
+      )
+      .then(
+        () => {
+          // Show message and add color
+          contactMessage.classList.add("color-green");
+          contactMessage.textContent = "You registered successfully";
+
+          // remove message after 3 seconds
+          setTimeout(() => {
+            contactMessage.textContent = "";
+          }, 3000);
+        },
+        (error) => {
+          // Mail sending error
+          alert("OOPS! SOMETHING HAS FAILED....", error);
+        }
+      );
+
+    // clear the input field
+    contactUser.value = "";
+  }
+};
+
+contactForm.addEventListener("submit", sendEmail);
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionClass = document.querySelector(".nav__menu a[href*=" + sectionId);
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionClass.classList.add("active-link");
+    } else {
+      sectionClass.classList.remove("active-link");
+    }
+  });
+};
+
+window.addEventListener("scroll", scrollActive);
+
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
